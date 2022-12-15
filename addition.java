619d0cfeed4b5b2c953057679ec5f48371920d4a
OmkarSatav <omkar7v2@gmail.com>
2022-12-15 21:40:47 +0530

    Addition File

---
+++ addition.java
@@ -0,0 +1,9 @@
+public class addition {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Addition of 10 and 15 is : " + add(10, 15));
+    }
+}
